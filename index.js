f8441cb7d1ba74cd8d8042f25fb4d036364e0ce2
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-06-02 18:44:15 +0600

    implemented users routes

--- index.js
+++ index.js
@@ -37,7 +37,42 @@ const getAllTours = (req, res) => {
   });
 };
 
+const getAllUsers = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: "success",
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
 const getTour = (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  // if (id > tours.length) {
+  //   return res.status(404).json({
+  //     status: "failed",
+  //     message: "Invalid Id",
+  //   });
+  // }
+  if (!tour) {
+    return res.status(404).json({
+      status: "failed",
+      message: "Invalid Id",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+};
+const getUser = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
@@ -75,8 +110,35 @@ const createTour = (req, res) => {
   );
   res.send("data posted");
 };
+const createUser = (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: "success", data: { tour: newTour } });
+    }
+  );
+  res.send("data posted");
+};
 
 const deleteTour = (req, res) => {
+  if (req.params.id > tours.length) {
+    return res.status(404).json({
+      status: "failed delete",
+      message: "Invalid Id",
+    });
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+};
+
+const deleteUser = (req, res) => {
   if (req.params.id > tours.length) {
     return res.status(404).json({
       status: "failed delete",
@@ -95,8 +157,13 @@ const deleteTour = (req, res) => {
 // app.get("/api/v1/tours/:id", getTour);
 // app.delete("/api/v1/tours/:id", deleteTour);
 
+// TOURES
 app.route("/api/v1/tours").get(getAllTours).post(createTour);
 app.route("/api/v1/tours/:id").get(getTour).delete(deleteTour);
+
+// USERS
+app.route("/api/v1/users").get(getAllUsers).post(createUser);
+app.route("/api/v1/users/:id").get(getUser).delete(deleteUser);
 
 app.listen(port, () => {
   console.log(`listening to ${port}`);
