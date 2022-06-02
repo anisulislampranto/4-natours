177d3849fe27f01b4f382ba96e7efa04abf53d41
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-06-02 16:51:13 +0600

    handling post req

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
 // app.get("/", (req, res) => {
 //   res.status(200).json({ text: "hello from the server side", app: "don" });
 // });
@@ -12,7 +14,37 @@ const port = process.env.PORT || 5000;
 //   res.send("post here");
 // })
 
-app.get("/api/v1/tours", (req, res) => {});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: "success", data: { tour: newTour } });
+    }
+  );
+
+  res.send("data posted");
+});
 
 app.listen(port, () => {
   console.log(`listening to ${port}`);
