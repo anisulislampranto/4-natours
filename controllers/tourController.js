2eed91dd230ace6f87a29dd9b2ba92dbb9b8c2be
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-06-04 16:30:59 +0600

    aggregation pipeline: matching and grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,58 +9,6 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-// class APIFeatures {
-//   constructor(query, queryString) {
-//     this.query = query;
-//     this.queryString = queryString;
-//   }
-
-//   filter() {
-//     const queryObj = { ...this.queryString };
-//     const excludedFeilds = ['page', 'sort', 'limit', 'fields'];
-//     excludedFeilds.forEach((el) => delete queryObj[el]);
-
-//     // (1B) advanced filtering
-//     let queryStr = JSON.stringify(queryObj);
-//     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-//     this.query.find(JSON.parse(queryStr));
-
-//     return this;
-//   }
-
-//   sort() {
-//     if (this.queryString.sort) {
-//       const sortBy = this.queryString.sort.split(',').join(' ');
-//       this.query = this.query.sort(sortBy);
-//       // sort('price ratingAvarage')
-//     } else {
-//       this.query = this.query.sort('-createdAt');
-//     }
-//     return this;
-//   }
-
-//   limitFields() {
-//     if (this.queryString.fields) {
-//       const fields = this.queryString.fields.split(',').join(' ');
-//       this.query = this.query.select(fields);
-//     } else {
-//       this.query = this.query.select('-__v');
-//     }
-//     return this;
-//   }
-
-//   panginate() {
-//     const page = this.queryString.page * 1 || 1;
-//     const limit = this.queryString.limit * 1 || 100;
-//     const skip = (page - 1) * limit;
-
-//     this.query = this.query.skip(skip).limit(limit);
-
-//     return this;
-//   }
-// }
-
 exports.getAllTours = async (req, res) => {
   try {
     // execute query
@@ -159,6 +107,46 @@ exports.deleteTour = async (req, res) => {
     res.status(204).json({
       status: 'success',
       data: null,
+    });
+  } catch (error) {
+    req.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          // _id: '$ratingsAverage',
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          mixPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
     });
   } catch (error) {
     req.status(404).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,6 +10,8 @@ router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
+router.route('/tour-states').get(tourController.getTourStats);
+
 router
   .route('/')
   .get(tourController.getAllTours)
