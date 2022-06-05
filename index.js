0dc74cafad2dbe0ffaecbb84d64bbfb3ac1c05d7
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-06-05 12:10:13 +0600

    better error and refectoring

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
+const globalErrorHandler = require('./controllers/errorControllers');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
@@ -40,21 +42,13 @@ app.all('*', (req, res, next) => {
   //     message: `Can't find ${req.originalUrl} on this server!`,
   //   });
 
-  const err = new Error(`Can't find ${req.originalUrl} on this`);
-  err.status = 'fail';
-  err.statusCode = 404;
+  // const err = new Error(`Can't find ${req.originalUrl} on this`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
