d1fd97fa1f1ea62a26614c9944dce8d0a5c8cb25
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-06-05 11:33:53 +0600

    global error handling middleware

--- index.js
+++ index.js
@@ -12,11 +12,9 @@ if (process.env.NODE_ENV === 'development') {
 
 // (1) MIDDLEWARE
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 // (1.1) CUSTOMIZE MEDDLEWARE
-app.use((req, res, next) => {
-  console.log('this is middlewere');
-  next();
-});
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -35,5 +33,28 @@ app.use((req, res, next) => {
 // MOUNTING NEW ROUTER FOR A ROUTE || MIDDLEWARE FOR ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  //   res.status(404).json({
+  //     status: 'fail',
+  //     message: `Can't find ${req.originalUrl} on this server!`,
+  //   });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { default: slugify } = require('slugify');
-const validator = require('validator');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
