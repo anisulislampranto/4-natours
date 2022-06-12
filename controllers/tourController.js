e01680b1c4c2043224963cfb0684859a8cd8cc1c
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-06-12 16:02:54 +0600

    virtual populate tours and reviews: didn't worked. pushing not working code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -47,7 +47,7 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   // try {
-  const tour = await Tour.findById({ _id: ObjectId(req.params.id) });
+  const tour = await Tour.findById(req.params.id).populate('reviews');
   // Tour.findOne({ _id: req.params.id });
 
   if (!tour) {
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -39,16 +39,22 @@ const reviewSchema = new mongoose.Schema(
 );
 
 reviewSchema.pre(/^find/, function (next) {
+  //   this.populate({
+  //     path: 'tour',
+  //     select: 'name',
+  //   }).populate({
+  //     path: 'user',
+  //     select: 'name',
+  //   });
+
   this.populate({
-    path: 'tour',
-    select: 'name',
-  }).populate({
     path: 'user',
     select: 'name photo',
   });
+
   next();
 });
 
-const Review = mongoose.model('Review ', reviewSchema);
+const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
