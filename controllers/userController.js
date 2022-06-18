8baffe1da3cb86c473d1e0ae22a0e3cee4c47811
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-06-18 11:25:21 +0600

    configured multer

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,41 @@
+const multer = require('multer');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const handlerFactory = require('./handlerFactory');
+
+// a complete defination of how we want to store our files with the destination and the filename
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // first arg is an error if there is one is not then just null
+    // second arg is the actual destination
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    // user-68767kah87-875756687.jpg
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+
+// in this function, the goal is basically to test if the uploaded file is an image
+// if it is so then we pass true into the above cb funtion line:17
+// if it is not then we pass false into the callback function along with an error
+// note: its not only for images its for all kinds of file depands on the application you are making
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Pleae upload only images', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadUserPhoto = upload.single('photo');
 
 const filterObj = (obj, ...alowedFeilds) => {
   const newObj = {};
