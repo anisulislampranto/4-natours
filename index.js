e964e00e2fd094e7d01d60c631bef1158c83a38e
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-07-07 18:14:41 +0600

    extending base template with blocks

--- index.js
+++ index.js
@@ -77,6 +77,18 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The forest hiker tour ',
+  });
+});
+
 // CREATING ROUTER
 
 // USERS
