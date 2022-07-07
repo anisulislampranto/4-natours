6ad572cd490a7bfe2f45f7e0e3ce362fe73ec9bd
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-07-07 14:45:39 +0600

    first step with pug

--- index.js
+++ index.js
@@ -71,7 +71,10 @@ app.use((req, res, next) => {
 
 // (3) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The forest hiker',
+    user: 'Jonas',
+  });
 });
 
 // CREATING ROUTER
